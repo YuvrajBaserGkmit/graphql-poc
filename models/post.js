ea7dc44a284d698221477edae179a581dedca4db
YuvrajBaserGkmit <yuvrajbaser@gkmit.co>
2024-08-05 18:35:04 +0530

    fix: add alias in user and post models

--- models/post.js
+++ models/post.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       this.belongsTo(models.User, {
         foreignKey: "user_id",
+        as: "user",
       });
     }
   }
